f74a26c45c1dfa45339aeddccc787abc336d79fb
Ben <btlmcculloch@gmail.com>
2025-07-22 12:29:54 +0000

    Connected S3 bucket to project to store scraped news

--- main.py
+++ main.py
@@ -2,28 +2,42 @@
 
 from scrapers.nrl import fetch_nrl_news
 from scrapers.zerotackle import fetch_zerotackle_news
+from utils.upload import upload_json_to_s3
 import json
+import boto3
 
 def main():
     print("Running main...")
 
-    # 1) Scrape NRL
+    # Scrape NRL
     nrl_news = fetch_nrl_news()
 
-    # 2) Scrape Zero Tackle (RSS)
+    # Scrape Zero Tackle (RSS)
     zerotackle_news = fetch_zerotackle_news()
 
-    # 3) Merge and sort
+    # Merge and sort
     all_news = nrl_news + zerotackle_news
     all_news.sort(key=lambda x: x['timestamp'], reverse=True)
 
-    # 4) Print combined output
+    # Print combined output
     print(f"Got {len(all_news)} total articles")
     print(json.dumps(all_news, indent=2))
 
-    # 5) Save to JSON
+    # Save to JSON
     with open("latest_nrl_news.json", "w") as f:
         json.dump(all_news, f, indent=2)
+
+
+    # Upload to s3 bucket
+    bucket_name = "nrl-bucket-latest-news"
+    key_name = "latest_nrl_news.json"
+    upload_json_to_s3(all_news, bucket_name, key_name)
+    print(f"Uploaded to S3: {bucket_name}/{key_name}")
+        
+
+
+
+
 
 if __name__ == "__main__":
     main()
--- run_scraper.sh
+++ run_scraper.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo "Starting scraper at $(date)"
+
 #Navigate to dir
 cd /home/ec2-user/Rugby-News-
 
 #Activate venv 
 source venv/bin/activate
 
+echo "Running scraper..."
+
 #Run main script with error logs 
-python3 main.py >> logs/scraper.log 2>&1
+python3 main.py >> logs/manual_run.log 2>&1
+
+echo "Python script finished running"
+
+echo "Finished scraper run at $(date)"
